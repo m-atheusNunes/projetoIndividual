69ae943b9680169f448e5b6955da3bc333140429
m-atheusNunes <matheus.nunes@sptech.school>
2023-06-03 14:04:25 -0300

    Mudando a cor da barra quando ela pertencer ao usuario logado

--- public/JS/ranking_script.js
+++ public/JS/ranking_script.js
@@ -28,9 +28,9 @@ const posicoes = [
 const dadosRanking = {
     labels: posicoes,
     datasets: [{
-        label: 'Movimentos utilizados para ganhar o jogo',
-        backgroundColor: '#DAA520',
-        borderColor: '#DAA520',
+        label: 'Movimentos utilizados para ganhar',
+        backgroundColor: [],
+        borderColor: '#7A5A09',
         data: []
     }]
 }
@@ -46,26 +46,35 @@ const myChart = new Chart(
     graficoRanking
     )
     
+var idUsuarioAtual = Number(sessionStorage.getItem("idUsuario"));
+
 async function plotarDados() {
     var dados = await reqDados()
     myChart.data.datasets[0].data = [];
     for(var i = 0; i < dados.length; i++) {
         myChart.data.datasets[0].data.push(dados[i].movUsados);
     }
+    for(var i = 0; i < dados.length; i++) {
+        var listaTentativas = await reqDados();
+        if(listaTentativas[i].fkUsuario == idUsuarioAtual) {
+            myChart.data.datasets[0].backgroundColor[i] = "#DAA520"
+        } else {
+            myChart.data.datasets[0].backgroundColor[i] = "#7A5A09"
+        }
+    }
     
     myChart.update();
 
     for(var i = 0; i <= 2; i++) {
         top3.innerHTML += `
-            <div class="card">
-                <div class="top3_content">
-                    <span class="posicao">${i+1}º LUGAR</span> <br>
-                    <p><span>Nome: </span> ${dados[i].nome}</p> 
-                    <p><span>Sobrenome: </span> ${dados[i].sobrenome}</p> 
-                    <p><span>Email: </span> ${dados[i].email}</p> 
-                    <p><span>Movimentos utilizados: </span> ${dados[i].movUsados}</p>
-                </div>
-            </div>
+        <div class="card">
+        <div class="top3_content">
+        <span class="posicao">${i+1}º LUGAR</span> <br>
+        <p><span>Nome: </span> ${dados[i].nome} ${dados[i].sobrenome}</p> 
+        <p><span>Email: </span> ${dados[i].email}</p> 
+        <p><span>Movimentos utilizados: </span> ${dados[i].movUsados}</p>
+        </div>
+        </div>
         `
     }
 }
